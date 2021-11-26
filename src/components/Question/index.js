1f67c5cc9d3ac377bf62c52cf4bcfb2dd818f1b7
Ethan Shealey <ethan.shealey@gmail.com>
2021-11-25 20:22:17 -0500

    fixed question text size on mobile

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,8 +1,12 @@
+import { useMediaQuery } from 'react-responsive';
 
 const Question = (props) => {
+
+    const isMobile = useMediaQuery({ maxWidth: 980 });
+
     return (
         <div className="header-container">
-            <h1 className="header-md">{ props.question }</h1>
+            <h1 className={ !isMobile ? 'header-md' : 'header' }>{ props.question }</h1>
             <h4 className="sub">{ props.questionCategory } | <span style={{ color: props.diffColor }}>{ props.questionDiff }</span></h4>
         </div>
     )
