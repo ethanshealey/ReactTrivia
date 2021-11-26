c56694dc02987a0e3936818f3ed7dcdc88245ec8
Ethan Shealey <ethan.shealey@gmail.com>
2021-11-25 20:39:35 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -18,15 +18,13 @@ function App() {
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} selectedKeys={selected.toString()}>
           <Menu.Item key="1" onClick={() => setSelected(1)}>Home</Menu.Item>
           <Menu.Item key="2" onClick={() => setSelected(2)}>Play</Menu.Item>
-          <Menu.Item key="3" onClick={() => setSelected(3)}>About</Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: '50px 50px 0 50px' }}>
         <div className="site-layout-content">
           { 
             selected === 1 ? <Home setSelected={setSelected} /> :
-            selected === 2 ? <Play /> :
-            selected === 3 && <h3>About</h3>
+            selected === 2 && <Play /> 
           }
         </div>
       </Content>
